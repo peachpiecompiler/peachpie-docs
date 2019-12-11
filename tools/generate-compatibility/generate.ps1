98e5756ea1d20ba19bd69b2159316dd1adc05bd7
Jakub Míšek <jmisek@gmail.com>
2019-12-11 18:45:49 +0100

    generate-compatibility tools

--- tools/generate-compatibility/generate.ps1
+++ tools/generate-compatibility/generate.ps1
@@ -1,9 +1,41 @@
 param (
 )
 
-# do not show dotnet welcome text
+function hr {
+    ""
+    "---"
+    ""
+}
+
+function progress {
+  param ([string]$title, [array]$php ,[array]$peachpie)
+
+  if (!$php) { return }
+  
+  # dfference of two arrays:
+  $diff = Compare-Object -ReferenceObject $php -DifferenceObject $peachpie
+
+  # filter only items present in $php and not present in $peachpie
+  # InputObject, SideIndicator
+  $missing = $diff | where SideIndicator -eq "<=" | Select-Object -ExpandProperty InputObject
+
+  # render progress
+  $progress = $php.count - $missing.count
+  "??? tiny ""[=$( [int](100 * $progress / $php.count) )% ""**$title** $progress / $($php.count)""]"""
+
+  if ($missing.count -eq 0) {
+    "    happy face"
+  } else {
+    foreach ($item in $missing) {
+      "    - ``$item``"
+    }
+  }
+}
+
+# do not show dotnet welcome text:
 [System.Environment]::SetEnvironmentVariable('DOTNET_SKIP_FIRST_TIME_EXPERIENCE', '1')
 
+# run collect features from php & peachpie:
 $output_php = & invoke-Expression "php -f program.php" | Out-String
 $output_peachpie = & invoke-Expression "dotnet run --no-logo" | Out-String
 
@@ -11,8 +43,28 @@ $php = $output_php | ConvertFrom-Json
 $peachpie = $output_peachpie | ConvertFrom-Json
 
 # prolog
-"Following table compares regular ``php`` and ``peachpie``:"
-"> PeachPie $($peachpie.version) vs. PHP $($php.version)"
+"!!! tip ""PeachPie $($peachpie.version) vs. PHP $($php.version)"""
+"    Following table compares defined extensions, functions and classes of a regular ``php`` against ``peachpie``.."
+
+hr
 
 # extensions
+progress "extensions" $php.extensions $peachpie.extensions
 
+""
+""
+
+$peachpie_all = @()
+
+foreach ($ext in $peachpie.extensions) {
+    $peachpie_all = $peachpie_all + $peachpie."ext-$ext"
+}
+
+foreach ($ext in $peachpie.extensions) {
+    #if ($php."ext-$ext")
+    progress $ext $php."ext-$ext" $peachpie_all #$peachpie."ext-$ext"
+}
+
+# overall
+
+# todo: missing functions/classes
